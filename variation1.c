52393926f3a3fd06d46c38a928bd8990c97bf1c1
Plate256 <minidemperor@gmail.com>
2025-09-01 12:22:58 +0800

    First Edit

--- variation1.c
+++ variation1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+typedef struct
+{
+    int elem[MAX];
+    int count;
+}List;
+List initialize (List L);
+List insertPos (List L, int data, int position);
+List deletePos (List L, int position);
+int Locate (List L, int data);
+List insertSorted (List L, int data);
+void Display (List L);
 int main (){
 printf ("test");
 printf ("AMBOT");
-//fuck this siht
 };
